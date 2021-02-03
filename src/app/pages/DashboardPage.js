238beff90c459878f7b35b462533f892617be102
aespina90 <contato@aespina.com>
2021-02-02 23:22:02 -0300

    Google Calendar Iframe

--- src/app/pages/DashboardPage.js
+++ src/app/pages/DashboardPage.js
@@ -2,6 +2,9 @@ import React from "react";
 
 export function DashboardPage() {
   return <>
-
+<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FSao_Paulo&amp;src=Y19xYTNkMHNkdGl1MWowNm9sbHF2aGY5c29zNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23D50000&amp;showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0"
+width="100%" height="100%" frameborder="0" scrolling="no"
+></iframe>
   </>;
 }
+
--- src/app/pages/MyPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {useSubheader} from "../../layout";
-
-export const MyPage = () => {
-  const suhbeader = useSubheader();
-  suhbeader.setTitle("My Custom title");
-
-  return (<>My Page</>);
-};
