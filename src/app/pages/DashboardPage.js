830b7491a340c28eba7db7a78e9ebecd990bcc5f
aespina90 <contato@aespina.com>
2021-02-03 02:54:06 -0300

    Google Calendar Correção

--- src/app/pages/DashboardPage.js
+++ src/app/pages/DashboardPage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export function DashboardPage() {
   return <>
-<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FSao_Paulo&amp;src=Y19xYTNkMHNkdGl1MWowNm9sbHF2aGY5c29zNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23D50000&amp;showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0"
+<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FSao_Paulo&amp;src=Y19xYTNkMHNkdGl1MWowNm9sbHF2aGY5c29zNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23D50000&amp;showTitle=0&amp;showTabs=1&amp;showTz=0&amp;showCalendars=0&amp;title=PorcelanaBall"
 width="100%" height="100%" frameborder="0" scrolling="no"
 ></iframe>
   </>;
